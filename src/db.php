540bb27360eaaa94f207893ba1bd219f47731ee5
Fernando Guerra <fguerra806@gmail.com>
2018-10-28 19:07:15 -0700

    db: Add database connection

---
+++ src/db.php
@@ -0,0 +1,19 @@
+<?php
+
+include 'credentials.php';
+
+$conn = oci_connect($username, $password, $dbserver);
+
+if($conn)
+{
+	print "<br> connection successful";
+}
+else
+{
+	$e = oci_error;
+	print "<br> connection failed";
+	print htmlentities($e[$message]);
+	exit;
+}
+
+?>
