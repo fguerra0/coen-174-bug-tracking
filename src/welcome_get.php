a33dbae24457ce6f170b4b15cc6530298ebcbeb4
Tim Shur <tim.shur@gmail.com>
2018-11-27 22:09:49 -0800

    add bug tracking id after form submission

--- src/welcome_get.php
+++ src/welcome_get.php
@@ -22,16 +22,12 @@
     $id = uniqid();
     $today = date('Y-m-d');
 
-
     $conn = db_connect();
-
     $query = "INSERT INTO Bugs (Bugid, LastName, FirstName, Email,
                                 Subject, Description, Status, DateSubmitted)
               VALUES ('$id', '$lastname', '$firstname', '$email',
                       '$subject', '$description', 'submitted', TO_DATE('$today', 'yyyy-mm-dd'))";
-
     insert_row($conn, $query);
-
     db_close($conn);
 
     ?>
@@ -64,7 +60,7 @@
     <div class="container col-md-8 col-md-offset-2">
         <div class="jumbotron">
             <h1 class="display-4">Thank you!</h1>
-            <p class="lead">We appreciate your bug report ~</p>
+            <p class="lead">We appreciate your bug report, your bug tracking ID is: <?php $id ?></p>
             <p class="lead">
                 <a class="btn btn-primary btn-lg" href="client.php" role="button">Okay!</a>
             </p>
